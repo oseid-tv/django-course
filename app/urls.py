7697060f557ff672a55278a8ebc54e87eb8ac412
super dev <superdev1031@gmail.com>
2025-02-22 15:06:58 -0400

    create CRUD using class view

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from app.views import home, create_article
+from app.views import (
+    ArticleListView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView
+)
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("articles/create/", create_article, name="create_article")
+    path("", ArticleListView.as_view(), name="home"),
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
+    path("<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete_article"),
 ]
