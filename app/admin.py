74d7bc3b586b6d819b5fcd377d857ae1f374962a
super dev <superdev1031@gmail.com>
2025-02-23 22:36:00 -0400

    customize django admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from app.models import Article, UserProfile
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "word_count", "status", "created_at", "updated_at")
+    list_filter = ("status",)
+    search_fields = ("title", "content")
+    date_hierarchy = "created_at"
+    ordering = ("created_at",)
+    readonly_fields = ("word_count", "created_at", "updated_at")
+
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(UserProfile)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import re
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -14,7 +16,7 @@ class UserProfile(AbstractUser):
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True, default="")
-    word_count = models.IntegerField()
+    word_count = models.IntegerField(blank=True, default="")
     twitter_post = models.TextField(blank=True, default="")
     status = models.CharField(
         max_length=20,
@@ -24,4 +26,7 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
+    def save(self, *args, **kwargs):
+        text = re.sub(r"<[^>]*>", "", self.content).replace("&nbsp;", " ")
+        self.word_count = len(re.findall(r"\b\w+\b", text))
+        super().save(*args, **kwargs)
