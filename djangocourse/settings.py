f3392b470245dece8275c68f5b575784a74aa2b4
super dev <superdev1031@gmail.com>
2025-03-09 17:01:41 -0400

    install tailwind css

--- djangocourse/settings.py
+++ djangocourse/settings.py
@@ -180,6 +180,10 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/**/*.html'],
+  content: [
+    './templates/**/*.html',
+    './**/templates/**/*.html',
+    './static/src/**/*.js',
+    './apps/*/templates/**/*.html', // If you use Django apps
+  ],
   theme: {
     extend: {},
   },
